4810ebc91bd5eef105038639b5e18e69afb6c81d
cukw <kan142006@gmail.com>
2026-02-18 10:09:24 +0300

    fix

--- Backend/services/NotificationService/Program.cs
+++ Backend/services/NotificationService/Program.cs
@@ -10,5 +10,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy", service = "NotificationService", timestamp = DateTime.UtcNow }));
 
 app.Run();
